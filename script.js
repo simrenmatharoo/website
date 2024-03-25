a2bd099f1e748e196757c5c085776948faaed7ec
unknown <simren.matharoo@gmail.com>
2024-03-25 13:59:06 -0400

    login and signup box

--- script.js
+++ script.js
@@ -10,8 +10,19 @@ var formattedDate = "Today's Date: " + monthName[month]+ " " + day + ', ' + year
 //display date
 document.getElementById('currentDate').textContent = formattedDate;
 
-//calendar ID: calendar
+//login/signup form 
+function toggleForm() {
+    var loginForm = document.getElementById("loginForm");
+    var signupForm = document.getElementById("signupForm");
+    var message = document.getElementById("message");
 
-
-
-;
+    if (loginForm.style.display === "none") {
+        loginForm.style.display = "block";
+        signupForm.style.display = "none";
+        message.innerHTML = "Don't have an account? <a href=\"#\" onclick=\"toggleForm()\">Sign Up</a>";
+    } else {
+        loginForm.style.display = "none";
+        signupForm.style.display = "block";
+        message.innerHTML = "Already have an account? <a href=\"#\" onclick=\"toggleForm()\">Login</a>";
+    }
+}
