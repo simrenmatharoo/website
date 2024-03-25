c899bd8cfbe7cf4c1be8df063e62c765e7356777
simrenmatharoo <35635891+simrenmatharoo@users.noreply.github.com>
2024-03-24 21:50:16 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // JavaScript code here
-});
+//Date ID: currentDate
+var currentDate = new Date();
+var day = currentDate.getDate();
+var month = currentDate.getMonth();
+var year = currentDate.getFullYear(); 
+var monthName =[
+    "January","February","March","April","May","June",
+    "July","August","September","October","November","December"];
+var formattedDate = "Today's Date: " + monthName[month]+ " " + day + ', ' + year; 
+//display date
+document.getElementById('currentDate').textContent = formattedDate;
+
+//calendar ID: calendar
+
+
+
+;
